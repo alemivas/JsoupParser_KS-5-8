a8b1711e64745140de2de3257ccd78d9394dbae5
Alexander <a_36524@mail.ru>
2025-09-05 06:04:34 +0400

    KS-5-8 fix tag & curly braces

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,11 +10,12 @@ fun main() {
     val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/" +
             "avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
 
-    val quotes: Elements = doc.select(".bzpNIu")
+    val quotes: Elements = doc.select("article")
 
     println("Цитаты из произведения \"Автостопом по галактике\" Дугласа Адамса:\n")
 
-    for (quote: Element in quotes)
+    for (quote: Element in quotes){
         println("${quote.text()}\n")
+    }
 
 }
